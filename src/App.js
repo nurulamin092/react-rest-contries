0405a803f6604bb2a96ebb2666d1bce8026d8bba
nurulamin092 <mnurulamin092@gmial.com>
2022-03-17 07:32:53 +0600

    show dada in display

--- src/App.js
+++ src/App.js
@@ -16,11 +16,24 @@ function LoadCountries() {
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => setCountrice(data));
   }, [])
   return (
     <div>
       <h1>Visiting Every countries of the world!!</h1>
+      <h3>Available Countries : {countries.length}</h3>
+      {
+        countries.map(country => <Countries name={country.name.common} region={country.region}></Countries>)
+      }
+    </div>
+
+  )
+}
+function Countries(props) {
+  return (
+    <div>
+      <h2>Name:{props.name}</h2>
+      <h2>Region:{props.region}</h2>
     </div>
   )
 }
